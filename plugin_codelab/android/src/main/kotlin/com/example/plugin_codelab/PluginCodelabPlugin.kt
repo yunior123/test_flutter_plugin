74442625d9d2c95c030475c045fb3e6364a225c1
yunior123 <yuniorrodriguezo460@gmail.com>
2022-02-06 01:49:25 -0500

    testing on android using kotlin

--- plugin_codelab/android/src/main/kotlin/com/example/plugin_codelab/PluginCodelabPlugin.kt
+++ plugin_codelab/android/src/main/kotlin/com/example/plugin_codelab/PluginCodelabPlugin.kt
@@ -2,40 +2,72 @@ package com.example.plugin_codelab
 
 import androidx.annotation.NonNull
 
-import io.flutter.embedding.engine.plugins.FlutterPlugin
-import io.flutter.plugin.common.MethodCall
-import io.flutter.plugin.common.MethodChannel
-import io.flutter.plugin.common.MethodChannel.MethodCallHandler
-import io.flutter.plugin.common.MethodChannel.Result
 
+import io.flutter.embedding.engine.plugins.FlutterPlugin;
+import io.flutter.plugin.common.BinaryMessenger;
+import io.flutter.plugin.common.MethodCall;
+import io.flutter.plugin.common.MethodChannel;
+import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
+import io.flutter.plugin.common.MethodChannel.Result;
+import io.flutter.plugin.common.PluginRegistry.Registrar;
+
+import io.flutter.plugin.common.MethodChannel.Result
+import com.sun.swing.internal.plaf.synth.resources.synth
+
+import java.util.ArrayList
+
+import jdk.internal.net.http.common.Log.channel
+import java.lang.Exception
+
+
+internal abstract class Synthesizer {
+  abstract fun start()
+  abstract fun stop()
+  abstract fun keyDown(key: Int): Int
+  abstract fun keyUp(key: Int): Int
+}
 /** PluginCodelabPlugin */
 class PluginCodelabPlugin: FlutterPlugin, MethodCallHandler {
   /// The MethodChannel that will the communication between Flutter and native Android
   ///
   /// This local reference serves to register the plugin with the Flutter Engine and unregister it
   /// when the Flutter Engine is detached from the Activity
-  private lateinit var channel : MethodChannel
+  private var channel: MethodChannel? = null
+  private var synth: Synth? = null
+  private val channelName = "plugin_codelab"
 
-  private static void setup(PluginCodelabPlugin plugin, BinaryMessenger binaryMessenger) {
-    plugin.channel = new MethodChannel(binaryMessenger, channelName);
-    plugin.channel.setMethodCallHandler(plugin);
-    plugin.synth = new Synth();
-    plugin.synth.start();
-  }
-  print(sks );
-  override fun onAttachedToEngine(@NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
-    setup(this, flutterPluginBinding.getBinaryMessenger());
+  private fun setup(plugin: PluginCodelabPlugin, binaryMessenger: BinaryMessenger) {
+    plugin.channel = MethodChannel(binaryMessenger, channelName)
+    plugin.channel.setMethodCallHandler(plugin)
+    plugin.synth = Synth()
+    plugin.synth.start()
   }
 
-  override fun onMethodCall(@NonNull call: MethodCall, @NonNull result: Result) {
-    if (call.method == "getPlatformVersion") {
-      result.success("Android ${android.os.Build.VERSION.RELEASE}")
+  fun onAttachedToEngine(@NonNull flutterPluginBinding: FlutterPluginBinding) {
+    setup(this, flutterPluginBinding.getBinaryMessenger())
+  }
+
+  fun onMethodCall(@NonNull call: MethodCall, @NonNull result: Result) {
+    if (call.method.equals("getPlatformVersion")) {
+      result.success("Android " + android.os.Build.VERSION.RELEASE)
+    } else if (call.method.equals("onKeyDown")) {
+      try {
+        val arguments = call.arguments as ArrayList<*>
+        val numKeysDown: Int = synth.keyDown(arguments[0] as Int)
+        result.success(numKeysDown)
+      } catch (ex: Exception) {
+        result.error("1", ex.message, ex.stackTrace)
+      }
+    } else if (call.method.equals("onKeyUp")) {
+      try {
+        val arguments = call.arguments as ArrayList<*>
+        val numKeysDown: Int = synth.keyUp(arguments[0] as Int)
+        result.success(numKeysDown)
+      } catch (ex: Exception) {
+        result.error("1", ex.message, ex.stackTrace)
+      }
     } else {
       result.notImplemented()
     }
-  }
-
-  override fun onDetachedFromEngine(@NonNull binding: FlutterPlugin.FlutterPluginBinding) {
-    channel.setMethodCallHandler(null)
   }
 }
